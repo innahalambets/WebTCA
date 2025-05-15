8dba77dbabbf221d37f34eaeb5a918fc0409a613
innahalambets <innah@tommycarwash.com>
2025-05-15 10:45:48 -0400

    utils

---
+++ src/test/java/com/dev/WebPortal/pages/WebPortalHomePage.java
@@ -0,0 +1,75 @@
+package com.dev.WebPortal.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import utils.BrowserUtils;
+
+public class WebPortalHomePage {
+
+    public WebPortalHomePage(WebDriver driver){
+
+        PageFactory.initElements(driver,this);
+
+
+    }
+
+    @FindBy(xpath = "//h1")
+    WebElement header;
+
+    @FindBy (xpath = "//a[@class='button white']")
+    WebElement signUpButtonTop;
+
+    @FindBy (xpath = "//a[@class='button']")
+    WebElement logInButtonTop;
+
+    @FindBy (xpath = "//input[@name='loginInput']")
+    WebElement loginInputField;
+
+    @FindBy (xpath = "//input[@type='submit']")
+    WebElement logInButton;
+
+    @FindBy (xpath = "//a[.='Sign up now']")
+    WebElement signUpNowButton;
+
+    @FindBy (xpath = "//a[.='Sign In']")
+    WebElement signInButton;
+
+    @FindBy (xpath = "//input[@type='submit']")
+    WebElement createAccountButton;
+
+    @FindBy (xpath = "//p[contains(text(), 'Already have an account?')]")
+    WebElement alreadyHaveAnAccountText;
+
+    @FindBy (xpath = "//p[contains(text(), 'New to TommyClub?')]")
+    WebElement newToTommyClubText;
+
+    public void homePageInformation(WebDriver driver, String expectedHeader, String expectedTitle, String expectedUrl){
+        Assert.assertEquals(BrowserUtils.getText(header), expectedHeader);
+        Assert.assertTrue(signUpButtonTop.isDisplayed() && signUpButtonTop.isEnabled());
+        Assert.assertTrue(logInButtonTop.isDisplayed() && logInButtonTop.isEnabled());
+        Assert.assertTrue(signUpNowButton.isDisplayed() && signUpNowButton.isEnabled());
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+
+
+    }
+
+    public void signUpNow(WebDriver driver, String expectedSignUpText, String loginInput){
+
+        Assert.assertEquals(alreadyHaveAnAccountText,expectedSignUpText );
+        Assert.assertTrue(signUpNowButton.isDisplayed() && signUpNowButton.isEnabled());
+        signUpNowButton.click();
+        loginInputField.sendKeys(loginInput);
+        createAccountButton.click();
+
+    }
+
+
+
+
+
+}
+
---
+++ src/test/java/com/dev/WebPortal/tests/WebPortalHomeTest.java
@@ -0,0 +1,39 @@
+package com.dev.WebPortal.tests;
+
+import com.dev.WebPortal.pages.WebPortalHomePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class WebPortalHomeTest {
+
+    @Test
+
+    public void validateHomePageInformation(){
+
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://portal.dev-tommycarwash.com/login");
+        WebPortalHomePage webPortalHomePage= new WebPortalHomePage(driver);
+        webPortalHomePage.homePageInformation(driver,"Welcome to the Club",
+                "Tommy's Express Member Portal", "https://portal.dev-tommycarwash.com/login");
+
+
+    }
+
+    @Test
+    public  void validateSignUpNow(){
+
+    WebDriver driver= new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    driver.get("https://portal.dev-tommycarwash.com/login");
+    WebPortalHomePage webPortalHomePage= new WebPortalHomePage(driver);
+    webPortalHomePage.signUpNow(driver, "New to TommyClub?", "innah+registration@tommycarwash.com");
+
+}
+
+}
